a58ad250fa484b67299a70b017690fba4f5b7706
Chris Parnin <chris.parnin@gmail.com>
2017-10-10 13:28:37 -0400

    code

--- Pipeline/jenkins.js
+++ Pipeline/jenkins.js
@@ -1,5 +1,6 @@
 const child_process = require('child_process')
 const http = require('http');
+const fs = require('fs')
 
 const commitFuzzedCode = (master_sha1, n) => {
     child_process.execSync(`git stash && git checkout fuzzer && git checkout stash -- . && git commit -am "Fuzzing master:${master_sha1}: # ${n}" && git push`)
@@ -12,6 +13,14 @@ const revertToFirstCommit = (firstSha1, n) => {
     child_process.execSync(`git checkout ${firstSha1}`)
 }
 
+const getInfo = (dest) => {
+    if( !fs.existsSync(dest) )
+        throw new Error('invalid path provided');
+    let branch = child_process.execSync(`cd ${dest} && git rev-parse --abbrev-ref HEAD`).toString().trim();
+    let sha1   = child_process.execSync(`cd ${dest} && git rev-parse master`).toString().trim();
+    return {branch: branch, sha1: sha1}
+}
+
 const triggerJenkinsBuild = (jenkinsIP, jenkinsToken, githubURL, sha1) => {
     try {
         child_process.execSync(`curl "http://${jenkinsIP}:8080/git/notifyCommit?url=${githubURL}&branches=fuzzer&sha1=${sha1}"`)
@@ -22,5 +31,6 @@ const triggerJenkinsBuild = (jenkinsIP, jenkinsToken, githubURL, sha1) => {
 }
 
 exports.commitFuzzedCode = commitFuzzedCode;
+exports.getInfo = getInfo;
 exports.revertToFirstCommit = revertToFirstCommit;
 exports.triggerJenkinsBuild = triggerJenkinsBuild;
--- Pipeline/test/simple.js
+++ Pipeline/test/simple.js
@@ -7,6 +7,20 @@ var fuzzer = require("../fuzzer.js");
 
 describe('testMain', function()
 {
+    describe('#repo', function()
+    {
+        it('should check branch and master', function() {
+            var info = pipeline.getInfo('test/resources')
+            // master 3ee83e99983b5541b8b7ee558a84a11191e4db49
+            expect(info.branch).to.be.equals("master")
+            expect(info.sha1).to.be.equals("3ee83e99983b5541b8b7ee558a84a11191e4db49")
+        });
+        it('should fail on non-paths', function() 
+        {
+            expect( pipeline.getInfo.bind(pipeline, "/ && ls -l") ).to.throw('invalid path provided');
+        });
+    });
+
     describe('#run()', function()
     {
         it('should find java files and change them', function(done) {
