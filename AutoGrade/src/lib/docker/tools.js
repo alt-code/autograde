512d3790dd5e7f2c63b9cd8d52a8ae3561e17cc2
Chris Parnin <chris.parnin@gmail.com>
2018-02-23 21:43:28 -0500

    dockerode

--- AutoGrade/src/lib/docker/tools.js
+++ AutoGrade/src/lib/docker/tools.js
@@ -1,14 +1,24 @@
 const child_process = require('child_process');
+var Docker = require('dockerode');
 
 class Tools {
 
     constructor() {
+        this.docker = new Docker({socketPath: '/var/run/docker.sock'});
     }
 
     async getContainerIp(name) {
-        let ip = await child_process.execSync(`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${name}`).toString();
-        return ip.trim();
+        var container = this.docker.getContainer(name);
+        let data = await container.inspect();
+        return data.NetworkSettings.IPAddress;
     }
+
+    async run(name)
+    {
+        //this.docker.run(testImage, ['bash', '-c', 'uname -a'], process.stdout)
+        //child_process.execSync(`docker run --name ${hw.id}-${host} -d -it phusion/passenger-full:latest /bin/bash`);
+    }
+
 }
 
 // Export factory class
